241a53ef0f6d50b92b6a0ea0f87d7469575ea99a
NODA Hiroyuki <hiroyuki.noda.1203@gmail.com>
2019-02-17 13:56:33 +0900

    User クラスの拡張方法を include から prepend に変更。

--- lib/redmine_append_users_display_format/patches/user_patch.rb
+++ lib/redmine_append_users_display_format/patches/user_patch.rb
@@ -2,13 +2,9 @@ require_dependency 'user'
 
 module RedmineAppendUsersDisplayFormat::Patches::UserPatch
 
-  def self.included(base)
-
+  def self.prepended(base)
     base.extend(ClassMethods)
-    base.send(:include, InstanceMethods)
- 
     base.class_eval do
-
       const_set :USER_FORMATS,
       {
         :username_firstname_lastname => {
@@ -142,11 +138,7 @@ module RedmineAppendUsersDisplayFormat::Patches::UserPatch
             :setting_order => 2004
           },
       }.merge(remove_const :USER_FORMATS)
-
-      private :exist_affiliation_users_custom_field
-
     end
-
   end
 
   module ClassMethods
@@ -181,49 +173,47 @@ module RedmineAppendUsersDisplayFormat::Patches::UserPatch
 
   end
 
-  module InstanceMethods
+  def affiliation
+    return @affiliation if @affiliation
+    @affiliation = nil
+    return '' unless exist_affiliation_users_custom_field
+    custom_values = CustomValue.
+                     joins(:custom_field).
+                     where(["#{CustomValue.table_name}.customized_id = ?", self.id]).
+                     where(["#{CustomValue.table_name}.customized_type = ?", 'Principal']).
+                     where(["#{CustomField.table_name}.type = ?", 'UserCustomField']).
+                     where(["#{CustomField.table_name}.name = ?", self.class.users_custom_fields_name_of_affiliation])
+    custom_value = custom_values.first
+    return self.class.independent_affiliation if custom_values.length != 1 or custom_value[:value].to_s.empty?
+    @affiliation = custom_value[:value]
+  end
 
-    def affiliation
-      return @affiliation if @affiliation
-      @affiliation = nil
-      return '' unless exist_affiliation_users_custom_field
-      custom_values = CustomValue.
-                       joins(:custom_field).
-                       where(["#{CustomValue.table_name}.customized_id = ?", self.id]).
-                       where(["#{CustomValue.table_name}.customized_type = ?", 'Principal']).
-                       where(["#{CustomField.table_name}.type = ?", 'UserCustomField']).
-                       where(["#{CustomField.table_name}.name = ?", self.class.users_custom_fields_name_of_affiliation])
-      custom_value = custom_values.first
-      return self.class.independent_affiliation if custom_values.length != 1 or custom_value[:value].to_s.empty?
-      @affiliation = custom_value[:value]
-    end
+  def exist_affiliation_users_custom_field
+    @exist_affiliation_users_custom_field ||=
+      ! self.class.users_custom_fields_name_of_affiliation.empty? &&
+      UserCustomField.where(name: self.class.users_custom_fields_name_of_affiliation).count == 1
+  end
 
-    def exist_affiliation_users_custom_field
-      @exist_affiliation_users_custom_field ||=
-        ! self.class.users_custom_fields_name_of_affiliation.empty? &&
-        UserCustomField.where(name: self.class.users_custom_fields_name_of_affiliation).count == 1
-    end
+  private :exist_affiliation_users_custom_field
 
-    # 名前の略称
-    # * Redmineに同じ姓のユーザーが存在しない場合、空白を返す
-    # * Redmineに同じ姓のユーザーが存在する場合
-    #   * 名前の1文字目が同じユーザーが存在しない場合、「名前の前の文字列 + 名前の1文字目 + 名前の後の文字列 」を返す
-    #   * 名前の1文字目が同じユーザーが存在する場合、諦めて「名前」を返す
-    def abbreviated_firstname
-      return @abbreviated_firstname if @abbreviated_firstname
-      u = User
-      return @abbreviated_firstname = '' if u.where(lastname: self[:lastname]).count == 1
-      before = Setting.plugin_redmine_append_users_display_format['string_before_abbreviated_firstname'].to_s
-      after  = Setting.plugin_redmine_append_users_display_format['string_after_abbreviated_firstname'].to_s
-      return @abbreviated_firstname = "#{before}#{self[:firstname][0]}#{after}" if
-        u.
-        where(lastname: self[:lastname]).
-        where(u.arel_table[:firstname].matches("#{self[:firstname][0]}%")).count == 1
-      @abbreviated_firstname = " #{self[:firstname]}"
-    end
-
+  # 名前の略称
+  # * Redmineに同じ姓のユーザーが存在しない場合、空白を返す
+  # * Redmineに同じ姓のユーザーが存在する場合
+  #   * 名前の1文字目が同じユーザーが存在しない場合、「名前の前の文字列 + 名前の1文字目 + 名前の後の文字列 」を返す
+  #   * 名前の1文字目が同じユーザーが存在する場合、諦めて「名前」を返す
+  def abbreviated_firstname
+    return @abbreviated_firstname if @abbreviated_firstname
+    u = User
+    return @abbreviated_firstname = '' if u.where(lastname: self[:lastname]).count == 1
+    before = Setting.plugin_redmine_append_users_display_format['string_before_abbreviated_firstname'].to_s
+    after  = Setting.plugin_redmine_append_users_display_format['string_after_abbreviated_firstname'].to_s
+    return @abbreviated_firstname = "#{before}#{self[:firstname][0]}#{after}" if
+      u.
+      where(lastname: self[:lastname]).
+      where(u.arel_table[:firstname].matches("#{self[:firstname][0]}%")).count == 1
+    @abbreviated_firstname = " #{self[:firstname]}"
   end
 
 end
 
-User.send :include, RedmineAppendUsersDisplayFormat::Patches::UserPatch
+User.prepend RedmineAppendUsersDisplayFormat::Patches::UserPatch
