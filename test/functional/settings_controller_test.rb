a8ee0a5080782afda78ade03506d168969a3ded7
NODA Hiroyuki <hiroyuki.noda.1203@gmail.com>
2019-02-24 20:38:45 +0900

    functional test 実装。

---
+++ test/functional/settings_controller_test.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class SettingsControllerTest < Redmine::ControllerTest
+  include Redmine::I18n
+
+  plugin_fixtures :users, :custom_fields, :custom_values
+
+  def setup
+    User.current = nil
+    @request.session[:user_id] = 1 # admin
+  end
+
+  def teardown
+    Setting.delete_all
+    Setting.clear_cache
+  end
+
+  def test_index
+    get :index
+    assert_response :success
+
+    assert_select 'select#settings_user_format' do
+      assert_select 'option[value=username_affiliation_firstname_lastname]', :text => 'admin : [Affiliation] Redmine Admin'
+      assert_select 'option[value=lastname_abbreviated_firstname]', :text => 'Admin(R)'
+      assert_select 'option[value=affiliation_username_lastname_abbreviated_firstname]', :text => '[Affiliation] admin : Admin(R)'
+    end
+  end
+
+end
